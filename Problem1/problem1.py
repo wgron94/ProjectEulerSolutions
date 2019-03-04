c1960c6cdd4b3ed355baa1ad2588b5627500f170
Wyatt Gronnemose <w.gron94@gmail.com>
2019-03-03 20:16:50 -0700

    Update problem1.py
    Refactored names

--- Problem1/problem1.py
+++ Problem1/problem1.py
@@ -1,7 +1,7 @@
-sum = 0;
+sum_of_multiples = 0;
 
 for i in range( 1, 1000 ):
     if i % 3 == 0 or i % 5 == 0:
-        sum += i
+        sum_of_multiples += i
 
-print( f"The sum of all multiples of 3 or 5 below 1000 is {sum}" );
+print( f"The sum of all multiples of 3 or 5 below 1000 is {sum_of_multiples}" );
